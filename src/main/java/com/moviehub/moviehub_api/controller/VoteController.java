504fec5df6627cc4a948dc0be30a5a110c9da329
Eliezer Bittencourt <likobittencourt@gmail.com>
2025-12-19 10:08:47 -0300

    fix: remove imports never used

--- src/main/java/com/moviehub/moviehub_api/controller/VoteController.java
+++ src/main/java/com/moviehub/moviehub_api/controller/VoteController.java
@@ -1,12 +1,9 @@
 package com.moviehub.moviehub_api.controller;
 
-import com.moviehub.moviehub_api.domain.entity.User;
 import com.moviehub.moviehub_api.dto.vote.VoteRequestDTO;
 import com.moviehub.moviehub_api.service.VoteService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import com.moviehub.moviehub_api.security.AuthenticatedUserService;
 
